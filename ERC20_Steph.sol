aff125a45d604730f1abf8c86732de45ec0a20e2
Padme <88598099+sgnovo8@users.noreply.github.com>
2022-08-25 12:34:39 -0400

    Update ERC20_Steph.sol

--- ERC20_Steph.sol
+++ ERC20_Steph.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract GLDToken is ERC20 {
-    constructor(uint256 initialSupply) public ERC20("Gold", "GLD") {
+contract STEFToken is ERC20 {
+    constructor(uint256 initialSupply) public ERC20("Stef", "STF") {
         _mint(msg.sender, initialSupply);
     }
 }
